96391722ec17f792bd7d4caf1a9e517735b9d05a
jundymek <jundymek@gmail.com>
2019-07-13 00:04:28 +0200

    feat: save/load to/from localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,35 @@
 "use strict";
 
-import moment from 'moment';
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+const firstInput = document.querySelector('.form-field__input--js')
+let firstNameValue = firstInput.value
+const firstNameField = firstInput.name
+
+const secondInput = document.querySelector('.form-field__input--js1')
+let lastNameValue = secondInput.value
+const lastNameField = secondInput.name
+
+firstInput.addEventListener('keyup', (e) => {
+    firstNameValue = e.target.value;
+    console.log(firstNameValue)
+})
+
+secondInput.addEventListener('keyup', (e) => {
+    lastNameValue = e.target.value;
+    
+    console.log(lastNameValue)
+})
+
+const toLocalStorage = document.querySelector('.button--save--js');
+toLocalStorage.addEventListener('click', () => {
+    console.log(toLocalStorage)
+    localStorage.setItem(firstNameField, firstNameValue);
+    localStorage.setItem(lastNameField, lastNameValue);
+});
+
+const fromLocalStorage = document.querySelector('.button--load--js')
+fromLocalStorage.addEventListener('click', () => {
+    const items = {...localStorage}
+    firstInput.value = localStorage.getItem(firstNameField)
+})
+// localStorage.clear()
