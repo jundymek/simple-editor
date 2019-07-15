cce2e2c9c58f5dfd2c38a1f24e7e16f57e0db94b
jundymek <jundymek@gmail.com>
2019-07-15 23:54:58 +0200

    feat: clear form fields after clear localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,60 @@
 "use strict";
 
-
-const firstInput = document.querySelector('.form-field__input--js');
+const firstInput = document.querySelector(".form-field__input--js");
 let firstNameValue = firstInput.value;
 const firstNameField = firstInput.name;
 
-const secondInput = document.querySelector('.form-field__input--js1');
+const secondInput = document.querySelector(".form-field__input--js1");
 let lastNameValue = secondInput.value;
 const lastNameField = secondInput.name;
 
-firstInput.addEventListener('keyup', (e) => {
+firstInput.addEventListener("keyup", e => {
     firstNameValue = e.target.value;
-})
+});
 
-secondInput.addEventListener('keyup', (e) => {
+secondInput.addEventListener("keyup", e => {
     lastNameValue = e.target.value;
-})
+});
 
-const toLocalStorage = document.querySelector('.button--save-js');
-toLocalStorage.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log(toLocalStorage)
+const toLocalStorage = document.querySelector(".button--save-js");
+toLocalStorage.addEventListener("click", e => {
+    e.preventDefault();
+    console.log(toLocalStorage);
     localStorage.setItem(firstNameField, firstNameValue);
     localStorage.setItem(lastNameField, lastNameValue);
 });
 
-
-const backside = document.querySelector('.main');
-const fromLocalStorage = document.querySelector('.button--load-js')
-const tableFirstValue = document.querySelector('.table__td--js')
-const tableSecondValue = document.querySelector('.table__td--second-js')
-fromLocalStorage.addEventListener('click', (e) => {
-    const tableFirstItem = document.querySelector('.table__th--js')
-    const tableSecondItem = document.querySelector('.table__th--second-js')
-    e.preventDefault()
+const backside = document.querySelector(".main");
+const fromLocalStorage = document.querySelector(".button--load-js");
+const tableFirstValue = document.querySelector(".table__td--js");
+const tableSecondValue = document.querySelector(".table__td--second-js");
+fromLocalStorage.addEventListener("click", e => {
+    const tableFirstItem = document.querySelector(".table__th--js");
+    const tableSecondItem = document.querySelector(".table__th--second-js");
+    e.preventDefault();
     firstNameValue = localStorage.getItem(firstNameField);
     lastNameValue = localStorage.getItem(lastNameField);
-    backside.classList.toggle('main--visible');
-    tableFirstItem.innerHTML = firstNameField
-    tableSecondItem.innerHTML = lastNameField
-    tableFirstValue.innerHTML = firstNameValue
-    tableSecondValue.innerHTML = lastNameValue
-})
+    backside.classList.toggle("main--visible");
+    tableFirstItem.innerHTML = firstNameField;
+    tableSecondItem.innerHTML = lastNameField;
+    tableFirstValue.innerHTML = firstNameValue;
+    tableSecondValue.innerHTML = lastNameValue;
+});
 
-const backsideExit = document.querySelector('.backside__exit--js')
-backsideExit.addEventListener('click', (e) => {
+const backsideExit = document.querySelector(".backside__exit--js");
+backsideExit.addEventListener("click", e => {
     e.preventDefault();
-    backside.classList.toggle('main--visible');
-})
+    backside.classList.toggle("main--visible");
+});
 
-const clearLocalstorage = document.querySelector('.button--clear-js')
-clearLocalstorage.addEventListener('click', (e) => {
+const clearLocalstorage = document.querySelector(".button--clear-js");
+clearLocalstorage.addEventListener("click", e => {
+    clearLocalstorage.classList.toggle("button--clear-shake");
     e.preventDefault();
     localStorage.clear();
-    tableFirstValue.innerHTML = '';
-    tableSecondValue.innerHTML = '';
-})
+    console.log(firstInput);
+    tableFirstValue.innerHTML = "";
+    tableSecondValue.innerHTML = "";
+    firstInput.value = "";
+    secondInput.value = "";
+});
